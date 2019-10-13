01bfc9ef412a88931ac0791caf5459086fbebf72
angerro <gloriana>
2019-10-14 01:05:54 +0300

    fix mail.php

--- web/mail.php
+++ web/mail.php
@@ -18,4 +18,4 @@ $input = file_get_contents('php://stdin');
 preg_match('|^To: (.*)|', $input, $matches);
 $t = $mail_path . $matches[1] . '_' . time() . '.eml';
 chmod($t, 0644);
-file_put_contents($t, $input);
+file_put_contents($t, $input, FILE_APPEND);
